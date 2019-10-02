4fb03613f7387427be2084f41754ca95c7bb8883
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-10-02 16:18:09 +0200

    32

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -9,8 +9,7 @@ class Calculator extends React.Component {
         soult: 0,
         idItem: 0,
         numb: 1,
-        numb2: 0,
-        allItemVal: 0,
+        ShowValueItem: 0,
         steamProv: 0,
         item: [],
         summary: 0,
@@ -25,6 +24,9 @@ class Calculator extends React.Component {
 
 
  money = () =>{
+   this.setState({
+    ShowValueItem: 0
+   })
     const cost = document.querySelector('#cost').value
     let val = document.querySelector('#quantity').value
     if(cost>0){
@@ -33,16 +35,13 @@ class Calculator extends React.Component {
         this.setState({
           soult: cost*(val)-cost*(val)*15/100,
           steamProv:cost*(val)*15/100,
-          numb2: 1,
-          allItemVal: 0
+          ShowValueItem: 1
         })
       }else{
         this.setState({
           soult: cost*(this.state.numb)-cost*(this.state.numb)*15/100,
           steamProv:cost*(this.state.numb)*15/100,
-          numb2: 1,
-          allItemVal: 0
-  
+          ShowValueItem: 1
         })
       }
       this.setState({
@@ -62,42 +61,51 @@ class Calculator extends React.Component {
    addItem = () =>{
     let cost = document.querySelector('#cost').value
     let val = document.querySelector('#quantity').value
+    let name = document.querySelector('#skinName').value
     cost = parseFloat(cost).toFixed(2)
-    if(cost > 0){
+    if(cost > 0 && name !== ''){
       if(val>0){
         val = parseInt(val)
-        const obj = {id: this.state.idItem,cena:cost,ilosc:val}
+        const obj = {id: this.state.idItem,nazwa: name,cena:cost,ilosc:val}
         this.state.item.push(obj)
         this.setState({
           item: this.state.item,
-          numb2: 0,
-          allItemVal: 0,
+          ShowValueItem: 0,
           idItem: this.state.idItem +1
         })
+        document.querySelector('#cost').value = ''
+        document.querySelector('#quantity').value = '' 
+        document.querySelector('#skinName').value = '' 
       }else{
-        const obj = {id: this.state.idItem,cena:cost,ilosc:'1'}
+        const obj = {id: this.state.idItem,nazwa: name,cena:cost,ilosc:'1'}
         this.state.item.push(obj)
         this.setState({
           item: this.state.item,
-          numb2: 0,
-          allItemVal: 0,
+          ShowValueItem: 0,
           idItem: this.state.idItem +1
         })
+        document.querySelector('#cost').value = ''
+        document.querySelector('#quantity').value = '' 
+        document.querySelector('#skinName').value = '' 
     }
     this.setState({
       error: ''
     })
     } else{
-      this.setState({
-        error: 'PODAJ CENE  ITEMU KTÓRY CHCESZ DODAĆ!!!'
-      })
+      if(name !== ''){
+        this.setState({
+          error: 'PODAJ CENE  ITEMU KTÓRY CHCESZ DODAĆ!!!'
+        })
+      }else{
+        this.setState({
+          error: 'PODAJ NAZWE SKINA !!!'
+        })
+      }
     }
-    document.querySelector('#cost').value = ''
-    document.querySelector('#quantity').value = '' 
+    this.moneyAll()
   }
 
   moneyAll = () =>{
-    if(this.state.item.length > 0){
       let sum = 0
       this.state.item.map(event =>{
         const sumOneItem = event.cena*event.ilosc
@@ -105,169 +113,231 @@ class Calculator extends React.Component {
         return 0;
      })
      this.setState({
-      summary:  sum ,
-      numb2: 0,
-      allItemVal: 1
+      summary:  sum 
     })
-    }else{
-      return 0;
-    }
-      
    }
    valueAllItem = ()=>{
-     if(this.state.summary > 0 && this.state.allItemVal >0){
        const soultAll = this.state.summary-this.state.summary*15/100
        const steamProvAll =  this.state.summary.toFixed(2)*15/100
       return(
         <>
+        <p className="Calculator-Resoult">Wynik:</p>
         <p>value all your item: {this.state.summary.toFixed(2)} {this.state.valute} </p>
         <p>soult all your item: {soultAll.toFixed(2)} {this.state.valute}</p>
         <p>steam provision all your item: {steamProvAll.toFixed(2)} {this.state.valute}</p>
         </>
        )
-     }else{
-       return '';
-     }
      
    }
 
    valueOneItem = () =>{
-     if(this.state.numb2>0){
+     if(this.state.ShowValueItem>0){
      return (
       <>
+        <span className="Calculator-Resoult">Wynik:</span>
         <p>soult: {this.state.soult.toFixed(2)} {this.state.valute}</p>
         <p>steam provision: {this.state.steamProv.toFixed(2)} {this.state.valute}</p>
       </>
      )
    }else{
-    return '';
+    return (
+      <>
+        <span className="Calculator-Resoult">Wynik:</span>
+        <p>soult: 0.00 {this.state.valute}</p>
+        <p>steam provision: 0.00 {this.state.valute}</p>
+      </>
+    )
    }
   }
 
   changeValute = (e) =>{
     const actualyValute = e.currentTarget.value
-    if(this.state.valute=="PLN" && actualyValute == 'EUR'){
+    if(this.state.valute==="PLN" && actualyValute === 'EUR'){
       const cost = document.querySelector('#cost').value/4.38
+      if(cost >0 || this.state.item.length > 0 ){
       document.querySelector('#cost').value =  cost
       this.state.item.map(event =>{
         const newCost = event.cena/4.38
         event.cena = newCost.toFixed(2)
      })
-    }else if (this.state.valute=="EUR" && actualyValute == 'PLN'){
+    }
+    }else if (this.state.valute==="EUR" && actualyValute === 'PLN'){
       const cost = document.querySelector('#cost').value*4.38
-      document.querySelector('#cost').value =  cost
-      this.state.item.map(event =>{
-        const newCost = event.cena*4.38
-        event.cena = newCost.toFixed(2)
-     })
-    }else if (this.state.valute=="PLN" && actualyValute == 'USD'){
+      if(cost >0 || this.state.item.length > 0 ){
+        document.querySelector('#cost').value =  cost
+        this.state.item.map(event =>{
+          const newCost = event.cena*4.38
+          event.cena = newCost.toFixed(2)
+       })
+      }
+    }else if (this.state.valute==="PLN" && actualyValute === 'USD'){
       const cost = document.querySelector('#cost').value/4.006
+      if(cost >0 || this.state.item.length > 0 ){
       document.querySelector('#cost').value =  cost
       this.state.item.map(event =>{
         const newCost = event.cena*0.249
         event.cena = newCost.toFixed(2)
      })
-    }else if (this.state.valute=="USD" && actualyValute == 'PLN'){
+    }
+    }else if (this.state.valute==="USD" && actualyValute === 'PLN'){
       const cost = document.querySelector('#cost').value*4.006
+      if(cost >0 || this.state.item.length > 0 ){
       document.querySelector('#cost').value =  cost
       this.state.item.map(event =>{
         const newCost = event.cena*4.006
         event.cena = newCost.toFixed(2)
      })
-    }else if (this.state.valute=="USD" && actualyValute == 'EUR'){
+    }
+    }else if (this.state.valute==="USD" && actualyValute === 'EUR'){
       const cost = document.querySelector('#cost').value/1.093
+      if(cost >0 || this.state.item.length > 0 ){
       document.querySelector('#cost').value =  cost
       this.state.item.map(event =>{
         const newCost = event.cena/1.093
         event.cena = newCost.toFixed(2)
      })
-    }else if (this.state.valute=="EUR" && actualyValute == 'USD'){
+    }
+    }else if (this.state.valute==="EUR" && actualyValute === 'USD'){
       const cost = document.querySelector('#cost').value*1.093
+      if(cost >0 || this.state.item.length > 0 ){
       document.querySelector('#cost').value =  cost
       this.state.item.map(event =>{
         const newCost = event.cena*1.093
         event.cena = newCost.toFixed(2)
      })
     }
+    }
     this.setState({
       valute: actualyValute,
-      allItemVal: 0
+      ShowValueItem: 0 
     })
     const cost = document.querySelector('#cost').value
-    if(cost > 0){
+    if(cost > 0 && this.state.soult >0){
       this.money()
     }
+
+    if(cost == 0){
+      document.querySelector('#cost').value = ''
+    }
+    if(this.state.item.length>0){
+      this.moneyAll()  
+    }
   }
+
+
   deleteOneItem = (id) =>{
     this.state.item.splice(id, 1)
     this.setState({
-      item: this.state.item
+      item: this.state.item,
     })
-  
+    this.moneyAll()
   }
-   deleteEFromInput  = () =>{
-    const cost = document.querySelector('#cost').value
-      console.log(cost.indexOf(2))
-     if(cost.indexOf(cost.keyCode !== 69) > -1){
-      console.log("bye")
-     }else{
-      console.log("elo")
-     }
+
+  deleteAllItem = () =>{
+    this.setState({
+      summary: 0,
+      item: []
+   })
+   this.valueAllItem()  
+  }
+ 
+   ChangeValuteInTable = (cost,e) =>{
+     console.log(cost)
+    
+    if(this.state.valute === "EUR"){
+      return(
+        2*4,2
+        )
+    }else{
+      return(
+        22*4,2
+      )
+    }
    }
 
-   deleteEFromInputt = (event) =>{
-   let cost =  event.currentTarget.value
-   let test = document.querySelector('#cost').value
-   console.log(test)
-   console.log(cost)
-   console.log(cost.length)
+   lookingForInput = (e) =>{
+    const value = e.currentTarget.value 
+    if(value === ''){
+      this.setState({
+        ShowValueItem:0
+      })
+    }else{
+      this.money()
+    }
    }
+
+
+
+
+
    render(){
      return(
-    <div>
-      <select className="valute-choise" onChange ={this.changeValute.bind(this)}>
-        <option>PLN</option>
-        <option>EUR</option>
-        <option>USD</option>
-      </select>
-      <table className= 'table-item'  border="0">
-        <tr>
-          <th>l.p.</th>
-        <th className="title-cost" >CENA</th>
-        <th className="title-quantity">ILOŚĆ</th>
-        </tr>
+    <div className= "Calculator" >
 
-          {this.state.item.map((event,index) => {
-            return(
-              <tr id={index + 1}   className={index%2===0?'color1': 'color2'}>
-            <td key={index} >{index+1}</td>
-            <td>{event.cena} {this.state.valute}</td>
-            <td>{event.ilosc} szt.</td>
-            <button onClick={this.deleteOneItem.bind(this,index)}>usuń</button>
-            </tr>
-            )
-          })}
-        
-       </table>
-      <input type='number' id='cost' placeholder="Cena" onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}/>
-      <span>x</span>
-      <input type='number' id='quantity' placeholder="1"   onKeyDown={(evt) => ["e", "E", "+", "-",",","."].includes(evt.key) && evt.preventDefault()}/>
-      <button onClick = {this.addItem.bind(this)}>dodaj item</button>
-      <button className= 'button-s'onClick={this.money.bind(this)}> Sprawdź</button>
-      <br/>
-      <h1 className= "errorCost">{this.state.error}</h1>
-      <br/>
-      <div>
-      {this.valueOneItem()}
-      </div>
+      <div className="Calculator-Form-Box">
 
+          <select className="Calculator-Valute-Choise" onChange ={this.changeValute.bind(this)}>
+            <option>PLN</option>
+            <option>EUR</option>
+            <option>USD</option>
+          </select>
+
+          <div className="Calculator-Form">
+            <span  className="Calculator-Form-Span">NAME SKIN</span>
+            <input type='text'  id='skinName' className="Calculator-Form-inputSkinName" maxlength="30" placeholder="Nazwa skina"  />
+            <span  className="Calculator-Form-Span">COST</span>
+            <input type='number'  id='cost' onChange={this.lookingForInput.bind(this)} className="Calculator-Form-inputCost" min='0' placeholder="Cena" onKeyDown={(evt) => ["e", "E", "+", "-"].includes(evt.key) && evt.preventDefault()}/>
+            <span  className="Calculator-Form-Span">QUANTITY</span>
+            <input type='number' id='quantity' onChange={this.money.bind(this)} className="Calculator-Form-inputQuantity" placeholder="1" min='1' onKeyDown={(evt) => ["e", "E", "+", "-",",","."].includes(evt.key) && evt.preventDefault()}/>
+            <span className= "errorCost">{this.state.error}</span>   
+            <button className="Calculator-Form-Button " onClick = {this.addItem.bind(this)}>ADD ITEM</button><br/>
+        </div>
       
-      <button onClick={this.moneyAll.bind(this)}> Sprawdź wszystkie</button>
-      <div id='Value-All-Item'>
-        {this.valueAllItem()}
+        <div className="Calculator-ValueOneItem">
+          {this.valueOneItem()}
+        </div>
+      </div>
+      <div className= 'Calculator-Table-Items' >
+        <table className='Calculator-Table-Form' border="1">
+          <tr>
+            <th className="Table-Form-Title-Index" >l.p.</th>
+            <th className="Table-Form-Title-NameSkin" >NAZWA SKINA</th>
+            <th className="Table-Form-Title-Cost" >CENA</th>
+            <th className="Table-Form-Title-Quantity">ILOŚĆ</th>
+            <th className="Table-Form-Title-Sumary">ŁĄCZNA WARTOŚĆ</th>
+            <th className="Table-Form-Title-Delete"> </th>
+          </tr>
+
+            {this.state.item.map((event,index) => {
+              return(
+                <tr id={index + 1}   className={index%2===0?'color1': 'color2'}>
+              <td key={index} className="Table-Index" >{index+1}</td>
+              <td className="Table-SkinName">{event.nazwa}</td>
+              <td className="Table-Cost">{event.cena} {this.state.valute}</td>
+              <td className="Table-Quantity">{event.ilosc} szt.</td>
+              <td className="Table-Sumary">{(event.cena*event.ilosc).toFixed(2)} {this.state.valute}</td>
+              <td className="Table-ButtonDelete"><button onClick={this.deleteOneItem.bind(this,index)}>DELETE</button></td>
+              </tr>
+              )
+            })}
+          
+        </table>
       </div>
       
-      
+      <div className="Calculator-Button-Box">
+      <div  onClick={this.deleteAllItem.bind(this)} className="svg-wrapper Calculator-deleteAll">
+      <svg height="40" width="150">
+         <text x="37" y="25" font-family="sans-serif" font-size="15px"  fill="white">DELETE ALL</text>
+         <rect id="shape" height="40" width="150" />  
+         </svg>
+      </div>    
+      </div>
+            <div className="Calculator-Quantity">
+              {this.valueAllItem()}
+            </div>
+            <div className="Calculator-LogoCS">
+            <img className="Calculator-LogoCS-IMG" src="/logo.png"/>
+            </div>
       
     </div>
      )
