3d64dc2c6a06c5b49f7213fd2d32ae04e979bde6
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-10-03 13:07:04 +0200

    ranks

--- src/components/Ranks.js
+++ src/components/Ranks.js
@@ -6,8 +6,63 @@ import { move } from './Service';
 function Ranks() {
   return (
     <div className="Ranks">
-     skins
-     <button className="Ranks-Back" onClick={()=>move()}>BACK</button>
+      <h1 className="Ranks-Box-Title">RANKS IN CS GO </h1>
+      <div className="Ranks-AllTable-Box">
+        <div className="Ranks-Table-Box MM-Table-Box">
+          <p className="Ranks-Table-Title MM-Title">MATCH MAKING</p>
+          <table className="Ranks-Table MM-Table " >
+            <tbody>
+              <tr>
+                <th>RANK IMG</th>
+                <th>RANK NAME</th>
+                <th>RANK FUNNY DESCRIPTION</th>
+              </tr>
+              <tr>
+                <td>OBRAZEK</td>
+                <td> NAZWA RANGI</td>
+                <td> NOOB PLAYER</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="Ranks-Table-Box Wingman-Table-Box">
+        <p className="Ranks-Table-Title Wingman-Title">WINGMAN</p>
+          <table className="Ranks-Table Wingman-Table ">
+            <tbody>
+              <tr>
+                <th>RANK IMG</th>
+                <th>RANK NAME</th>
+                <th>RANK FUNNY DESCRIPTION</th>
+              </tr>
+              <tr>
+                <td>OBRAZEK</td>
+                <td> NAZWA RANGI</td>
+                <td> NOOB PLAYER</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+
+        <div className="Ranks-Table-Box DZ-Table-Box">
+          <p className="Ranks-Table-Title DZ-Title">DANGER ZONE</p>
+          <table className="Ranks-Table DZ-Table ">
+            <tbody>
+              <tr>
+                <th>RANK IMG</th>
+                <th>RANK NAME</th>
+                <th>RANK FUNNY DESCRIPTION</th>
+              </tr>
+              <tr>
+                <td>OBRAZEK</td>
+                <td> NAZWA RANGI</td>
+                <td> NOOB PLAYER</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <button className="Ranks-Back" onClick={()=>move()}>BACK</button>
     </div>
   );
 }
