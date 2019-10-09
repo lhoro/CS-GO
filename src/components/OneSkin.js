a18a14431434ca16d701d4ae5d1fe61b19351d03
Ivenoo <32682666+Ivenoo@users.noreply.github.com>
2019-10-09 22:10:27 +0200

    actualization

---
+++ src/components/OneSkin.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const OneSkin = (props) =>{
+
+  return(
+<div className="One-Skin" key={props.index} >
+  <p className="One-Skin-Name" style={props.colorSkin}>{props.event.name_weapon} | {props.event.name_skin}</p>
+  <div className="Skins-img_float" >
+    <img id={props.event.name_weapon+props.event.name_skin}  className="Skins-img" src={props.event.img_skin} alt="sorry error this skin img"/>
+    <div className="Buttons-float">
+      {(props.event.cost_factory !== "Not Possible"?<button className="Skin-float-bt fn-button" onMouseOver={() => {props.float_skin(props.event,"fn")}} onMouseOut={()=>{props.def_float(props.event)}}>Factory New</button>:null)}
+      {(props.event.cost_minimal !== "Not Possible"?<button className="Skin-float-bt mw-button" onMouseOver={() => {props.float_skin(props.event,"mw")}} onMouseOut={()=>{props.def_float(props.event)}}>Minimal Wear</button>:null)}
+      {(props.event.cost_field !== "Not Possible"?<button className="Skin-float-bt ft-button" onMouseOver={() => {props.float_skin(props.event,"ft")}}   onMouseOut={()=>{props.def_float(props.event)}}>Field-Test</button>:null)}
+      {(props.event.cost_wellworm !== "Not Possible"?<button className="Skin-float-bt ww-button" onMouseOver={() => {props.float_skin(props.event,"ww")}}onMouseOut={()=>{props.def_float(props.event)}}>Well-Worm</button>:null)}
+      {(props.event.cost_battle !== "Not Possible"?<button className="Skin-float-bt bs-button" onMouseOver={() => {props.float_skin(props.event,"bs")}}  onMouseOut={()=>{props.def_float(props.event)}}>Battle_Scarred</button>:null)}
+    </div>
+  </div>
+  <div className="Skins-cost_float">
+    <p className="Skins-cost-shadow"><span>StatTrack</span> Factory New:<span className="price_skin">{props.event.st_cost_factory} $</span></p>
+    <p><span>StatTrack</span> Minimal Wear:<span className="price_skin">{props.event.st_cost_minimal} $</span></p>
+    <p className="Skins-cost-shadow"><span>StatTrack</span> Field-Test:<span className="price_skin">{props.event.st_cost_field} $</span></p>
+    <p ><span>StatTrack</span> Well-Worm:<span className="price_skin">{props.event.st_cost_wellworm} $</span></p>
+    <p className="Skins-cost-shadow"><span>StatTrack</span> Battle-Scarred:<span className="price_skin">{props.event.st_cost_battle} $</span></p>
+    <p>Factory New:<span className="price_skin">{props.event.cost_factory} $</span></p>
+    <p className="Skins-cost-shadow">Minimal Wear:<span className="price_skin">{props.event.cost_minimal} $</span></p>
+    <p>Field-Test:<span className="price_skin">{props.event.cost_field} $</span></p>
+    <p className="Skins-cost-shadow">Well-Worm:<span className="price_skin">{props.event.cost_wellworm}$</span></p>
+    <p>Battle-Scarred:<span className="price_skin">{props.event.cost_battle} $</span></p>
+  </div>
+</div>
+  )
+
+}
+export default OneSkin
--- src/components/Skins.js
+++ src/components/Skins.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './style/Skins.css';
 import skins from "../data/skins.json";
+import OneSkin from './OneSkin.js'
 
 import { move, float_skin, def_float } from './Service';
 
@@ -9,7 +10,6 @@ class Skins extends React.Component {
     super(props);
     this.state ={
       skins:[],
-      skinsSearch:[],
       searchValue:"",
     }
 }
@@ -26,29 +26,10 @@ componentDidMount(){
    this.setState({
      searchValue: valueSearch,
    })
-  
-    this.state.skins.map(event =>{
-      const nameSkinWeapon = event.name_weapon+event.name_skin;
-       if(event.name_skin.indexOf(valueSearch)>-1){
-          this.state.skinsSearch.push(event);
-          this.setState({
-            skinsSearch: this.state.skinsSearch,
-          })
-       }
-     })
-   
-  
   }
 
 
   render(){
-    let list = {};
-    if(this.state.searchValue===""){
-      list =  this.state.skins;
-    }
-    else{
-      list =  this.state.skinsSearch;
-    }
     return (
       <div className="Skins">
         <div className="Skins-Title">
@@ -59,37 +40,19 @@ componentDidMount(){
         </div>
         
         <div className="Skins-Body">
-        {list.map((event,index)=>{
+        {this.state.skins.map((event,index)=>{
           let colorSkin= {
             color: event.skin_color
           }
-          return(
-          <div className="One-Skin" key={index} >
-             <p className="One-Skin-Name" style={colorSkin}>{event.name_weapon} | {event.name_skin}</p>
-            <div className="Skins-img_float" >
-              <img id={event.name_weapon+event.name_skin}  className="Skins-img" src={event.img_skin} alt="sorry error this skin img"/>
-              <div className="Buttons-float">
-                  {(event.cost_factory !== "Not Possible"?<button className="Skin-float-bt fn-button" onMouseOver={() => {float_skin(event,"fn")}} onMouseOut={()=>{def_float(event)}}>Factory New</button>:null)}
-                  {(event.cost_minimal !== "Not Possible"?<button className="Skin-float-bt mw-button" onMouseOver={() => {float_skin(event,"mw")}} onMouseOut={()=>{def_float(event)}}>Minimal Wear</button>:null)}
-                  {(event.cost_field !== "Not Possible"?<button className="Skin-float-bt ft-button" onMouseOver={() => {float_skin(event,"ft")}}   onMouseOut={()=>{def_float(event)}}>Field-Test</button>:null)}
-                  {(event.cost_wellworm !== "Not Possible"?<button className="Skin-float-bt ww-button" onMouseOver={() => {float_skin(event,"ww")}}onMouseOut={()=>{def_float(event)}}>Well-Worm</button>:null)}
-                  {(event.cost_battle !== "Not Possible"?<button className="Skin-float-bt bs-button" onMouseOver={() => {float_skin(event,"bs")}}  onMouseOut={()=>{def_float(event)}}>Battle_Scarred</button>:null)}
-              </div>
-            </div>
-            <div className="Skins-cost_float">
-              <p className="Skins-cost-shadow"><span>StatTrack</span> Factory New:<span className="price_skin">{event.st_cost_factory} $</span></p>
-              <p><span>StatTrack</span> Minimal Wear:<span className="price_skin">{event.st_cost_minimal} $</span></p>
-              <p className="Skins-cost-shadow"><span>StatTrack</span> Field-Test:<span className="price_skin">{event.st_cost_field} $</span></p>
-              <p ><span>StatTrack</span> Well-Worm:<span className="price_skin">{event.st_cost_wellworm} $</span></p>
-              <p className="Skins-cost-shadow"><span>StatTrack</span> Battle-Scarred:<span className="price_skin">{event.st_cost_battle} $</span></p>
-              <p>Factory New:<span className="price_skin">{event.cost_factory} $</span></p>
-              <p className="Skins-cost-shadow">Minimal Wear:<span className="price_skin">{event.cost_minimal} $</span></p>
-              <p>Field-Test:<span className="price_skin">{event.cost_field} $</span></p>
-              <p className="Skins-cost-shadow">Well-Worm:<span className="price_skin">{event.cost_wellworm}$</span></p>
-              <p>Battle-Scarred:<span className="price_skin">{event.cost_battle} $</span></p>
-            </div>
-          </div>
-          )
+          const nameSkin = event.name_weapon+event.name_skin.toUpperCase()
+          if(nameSkin.indexOf(this.state.searchValue.toUpperCase()) > -1){
+            return(
+              <OneSkin event={event} index={index}  float_skin={float_skin}  def_float={def_float} colorSkin={colorSkin} />
+              )
+          }else{
+            return '';
+          }
+          
         })}
         </div>
   
@@ -100,3 +63,5 @@ componentDidMount(){
 }
 
 export default Skins;
+
+
