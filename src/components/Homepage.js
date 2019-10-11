e93c4459f376bce3808ecc802dd7647901cfdcfe
lhoro <32704441+lhoro@users.noreply.github.com>
2019-10-11 13:56:48 +0200

    up

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -9,9 +9,14 @@ function Homepage() {
       <div className="Image Image1"></div>
       <img src="/image/layout/arrowup.png" className="Arrow-Up" alt="arrow" onClick={()=>move("up")} />
       <div className="Image Image2"></div>
-      <div className="Empty"></div>
+      <div className="Empty">
+        <img src="/image/layout/arrowleft.png" className="Arrow-Left" alt="arrow" onClick={()=>move("left")} />
+        <img src="/image/layout/arrowright.png" className="Arrow-Right" alt="arrow" onClick={()=>move("right")} />
+      </div>
       <div className="Image Image3"></div>
+        <img src="/image/layout/arrowdown.png" className="Arrow-Down" alt="arrow" onClick={()=>move("down")} />
       <div className="Image Image4"></div>
+      <div className="Footer"></div>
 
       <div className="Circle">
         <div className="Animation-Box" >
@@ -21,11 +26,6 @@ function Homepage() {
           </div>
         </div>
       </div>
-      
-      
-      <img src="/image/layout/arrowdown.png" className="Arrow-Down" alt="arrow" onClick={()=>move("down")} />
-      <img src="/image/layout/arrowleft.png" className="Arrow-Left" alt="arrow" onClick={()=>move("left")} />
-      <img src="/image/layout/arrowright.png" className="Arrow-Right" alt="arrow" onClick={()=>move("right")} />
     </div>
   );
 }
